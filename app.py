9882b8eddfb354c831a24e856eefa409d44e44f2
Vitaly Bichov <vitalyb@wix.com>
2019-04-23 15:46:11 +0300

    adapted the app to python 3.7

--- app.py
+++ app.py
@@ -2,79 +2,74 @@ import socket
 from time import sleep
 import logging as log
 import threading
-from BaseHTTPServer import HTTPServer
+import random
 from flask import Flask, render_template, Response, request
-from prometheus_client import start_http_server, Summary, MetricsHandler, Counter, generate_latest
+from prometheus_client import start_http_server, Histogram, MetricsHandler, Counter, generate_latest
+from prometheus_client.exposition import _ThreadingSimpleServer, start_http_server
+_ThreadingSimpleServer.daemon_threads = True
+
 
 app = Flask(__name__)
 PROMETHEUS_PORT = 9000
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 
 # Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds', 'DESC: Time spent processing request')
-INDEX_TIME = Summary('index_request_processing_seconds', 'DESC: INDEX time spent processing request')
+REQUEST_TIME = Histogram('dummy_flask_request_time', 'DESC: Time spent processing request', ('method', 'endpoint'))
+ROOT_REQUEST_TIME = REQUEST_TIME.labels('GET', '/')
+# HOST_REQUEST_TIME = REQUEST_TIME.labels('GET', '/host')
 
 # Create a metric to cound the number of runs on process_request()
 c = Counter('requests_for_host', 'Number of runs of the process_request method', ['method', 'endpoint'])
 
 @app.route('/')
-@INDEX_TIME.time()
+@ROOT_REQUEST_TIME.time()
 def hello_world():
     path = str(request.path)
     verb = request.method
     label_dict = {"method": verb,
                  "endpoint": path}
     c.labels(**label_dict).inc()
-
     return 'Flask Dockerized'
 
 # Decorate function with metric.
 def process_request():
     """A dummy function that takes some time."""
-    sleep(2)
-
+    weithed_sleep_times = [0.005] * 1 + [0.01] * 3 + [0.025] * 5 + [0.05] * 7 + [0.075] * 5 + [0.1] * 4 + [0.25] * 2 + [0.5] + [0.75] + [1.0] + [2.5]
+    sleep(random.choice(weithed_sleep_times))
     fqdn = socket.getfqdn()
     return fqdn
 
 @app.route('/host')
-@REQUEST_TIME.time()
+# @HOST_REQUEST_TIME.time()
 def host():
     path = str(request.path)
     verb = request.method
     label_dict = {"method": verb,
                   "endpoint": path}
     c.labels(**label_dict).inc()
-
-    ret = str(process_request())
-    return "The name of this host is: {}".format(ret)
+    with REQUEST_TIME.labels(verb, path).time():
+        ret = str(process_request())
+        return "The name of this host is: {}".format(ret)
 
 @app.route('/metrics')
 def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
-# Seperate web server for prometheus scraping
-class PrometheusEndpointServer(threading.Thread):
-    """A thread class that holds an http and makes it serve_forever()."""
-    def __init__(self, httpd, *args, **kwargs):
-        self.httpd = httpd
-        super(PrometheusEndpointServer, self).__init__(*args, **kwargs)
 
-    def run(self):
-        self.httpd.serve_forever()
 
-# keep the server running
-def start_prometheus_server():
-    try:
-        httpd = HTTPServer(("0.0.0.0", PROMETHEUS_PORT), MetricsHandler)
-    except (OSError, socket.error):
-        return
+# # keep the server running
+# def start_prometheus_server():
+#     try:
+#         httpd = HTTPServer(("0.0.0.0", PROMETHEUS_PORT), MetricsHandler)
+#     except (OSError, socket.error):
+#         return
 
-    thread = PrometheusEndpointServer(httpd)
-    thread.daemon = True
-    thread.start()
-    log.info("Exporting Prometheus /metrics/ on port %s", PROMETHEUS_PORT)
+#     thread = PrometheusEndpointServer(httpd)
+#     thread.daemon = True
+#     thread.start()
+#     log.info("Exporting Prometheus /metrics/ on port %s", PROMETHEUS_PORT)
 
-start_prometheus_server()
+start_http_server(PROMETHEUS_PORT)
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0',threaded=True)
